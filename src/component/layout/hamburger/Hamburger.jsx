b42394233167926559f8e287fa82acc371b2bd1c
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2024-10-14 16:23:12 +0300

    add-ham

--- src/component/layout/hamburger/Hamburger.jsx
+++ src/component/layout/hamburger/Hamburger.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react'
+import { CgMenuRight } from 'react-icons/cg'
+import { IoClose } from 'react-icons/io5'
 import styles from './Hamburger.module.scss'
 const Hamburger = () => {
-	return <div className={styles.hamburger}>|||</div>
+	const [isVisible, setVisible] = useState(false)
+	return (
+		<div className={styles.wrapper}>
+			<button onClick={() => setVisible(!isVisible)}>
+				{isVisible ? <IoClose /> : <CgMenuRight />}
+			</button>
+			<div className={isVisible ? '' : styles.menu}>Menu</div>
+		</div>
+	)
 }
 export default Hamburger
